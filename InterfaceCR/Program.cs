7227289263edff8303c5d33a668e3477fec451e6
roubius <lautomasi035@gmail.com>
2023-06-01 15:01:44 -0300

    add InterfaceCR

---
+++ InterfaceCR/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using app.entities;
+
+namespace POO_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ControleRemoto CR = new ControleRemoto();
+
+            CR.Ligar();
+            CR.MaisVolume();
+            // CR.LigarMudo();
+            CR.Play();
+            CR.AbrirMenu();
+        }
+    }
+}
