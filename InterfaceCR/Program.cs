7e030d6109a740719e1e0265c41fbea159d1924f
roubius <lautomasi035@gmail.com>
2023-06-01 15:12:14 -0300

    update InterfaceCR

--- InterfaceCR/Program.cs
+++ InterfaceCR/Program.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using app.entities;
+﻿using app.entities;
 
 namespace POO_Interface
 {
@@ -18,6 +13,7 @@ namespace POO_Interface
             // CR.LigarMudo();
             CR.Play();
             CR.AbrirMenu();
+            System.Console.ReadKey();
         }
     }
 }
