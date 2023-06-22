f4d373077777855d1ecae1e17f4fed58e14d98c2
roubius <lautomasi035@gmail.com>
2023-06-22 15:16:27 -0300

    UPDATE BD_DELETE (COMPLETO)

--- CRUD_Console_NLM/BD_DELETE/Program.cs
+++ CRUD_Console_NLM/BD_DELETE/Program.cs
@@ -7,6 +7,21 @@ namespace BD_DELETE
     {
         static void Main(string[] args)
         {
+            OleDbConnection conexao = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0;
+                                                            Data Source = C:\Users\Técnico\Desktop\Nicolas\DB_Escola.mdb");
+
+            conexao.Open();
+
+            Console.Write("Digite o código do aluno que deseja excluir: ");
+            int codAluno = int.Parse(Console.ReadLine());
+
+            OleDbCommand sql_Delete = new OleDbCommand($"DELETE FROM tbAluno WHERE idAluno = {codAluno}", conexao);
+
+            sql_Delete.ExecuteNonQuery();
+
+            Console.WriteLine("Exclusão concluída com sucesso!");
+
+            conexao.Close();
         }
     }
 }
