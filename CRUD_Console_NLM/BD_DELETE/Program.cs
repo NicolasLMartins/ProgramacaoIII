ff300f3a39c836db6a68fa4f9e18dd04ffd37da4
roubius <lautomasi035@gmail.com>
2023-06-22 15:11:01 -0300

    ADD BD_UPDATE (COMPLETO) AND BD_DELETE (INCOMPLETO)

---
+++ CRUD_Console_NLM/BD_DELETE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace BD_DELETE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ CRUD_Console_NLM/BD_UPDATE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace BD_UPDATE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OleDbConnection conexao = new OleDbConnection(@"Provider = Microsoft.Jet.OLEDB.4.0;
+                                                            Data Source = C:\Users\Técnico\Desktop\Nicolas\DB_Escola.mdb");
+
+            conexao.Open();
+
+            Console.Write("Digite o código do aluno para editar seu registro: ");
+            int codAluno = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o novo nome ou o nome corrigido: ");
+            string nomeAluno = Console.ReadLine();
+
+            OleDbCommand sql_Update = new OleDbCommand($"UPDATE tbAluno SET nome = '{nomeAluno}' WHERE idAluno = {codAluno}", conexao);
+
+            sql_Update.ExecuteNonQuery();
+
+            Console.WriteLine("\nAtualização concluida com sucesso!");
+
+            conexao.Close();
+        }
+    }
+}
